b2c8996157c249d77015825dc94c4bacdd140eb5
Kateattardo <youremail@domain.com>
2023-08-07 11:20:29 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+//what are the constants//
+const card =[ "card a1", "card a2", "card b1", "card b2", "card c1", "card c2", "card d1", "card d2", "card e1", "card e2", "card f1" , "card f2"]
+
+//What are the variables://
+let flip = filpCard()
+let match = matchCard()
+let timer= timerCountDown ()
+
+
+// game variables: what changes in the game? score restults, wahat games as game continues?//
+
+
+
+//cached elements
+
+
+
+//functions//
+//flip cards
+//remove matches from the board
+//clock timer
+//reset button
+
+//render//
+
+for each loop
+
+
+
+
+
+//event listeners/
