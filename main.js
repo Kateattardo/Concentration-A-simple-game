cac3cfad7a223b6cdde64fb29bdd409b8feceef7
Kateattardo <youremail@domain.com>
2023-08-10 15:17:00 -0400

    main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //Constants
 
-document.addEventListener("DOMContentLoaded", function () {
     const cards = document.querySelectorAll(".dogcard");
     const timer = document.getElementById("timer");
     const button = document.querySelector("play-again-button");
     const wrongMessage = document.getElementById("wrong-message");
 
+    let gameStarted = false;
     let flippedCards = [];
     let matchedPairs = 0;
     let secondsRemaining = 120; // Initial time
@@ -13,23 +13,25 @@ document.addEventListener("DOMContentLoaded", function () {
     let lockedBoard = false;
     let unlockBoard = true;
 
-
-    // Shuffle cards "Fisher-Yates"
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [cards[i], cards[j]] = [cards[j], cards[i]];
+//flip cards
+    function flipCard(card) {
+        if (boardLocked || card.classList.contains("flipped")) {
+            return;
+        }
+        card.classList.add("flipped");
+        flippedCards.push(card);
+        if (flippedCards.length === 2) {
+            lockBoard();
+        if (checkMatching()) {
+                handleMatch();
+        } else {
+                setTimeout(() => {
+                resetFlippedCards();
+                unlockBoard();
+                }, 1000);
+            }
         }
     }
-    console.log(shuffleArray)
-
-    // Reset flipped cards if no match
-    function resetFlippedCards() {
-        flippedCards.forEach(card => card.classList.remove("flipped"));
-        flippedCards = [];
-    }
-    console.log(resetFlippedCards)
-    
 
     // Check if the flipped cards match
     function checkMatching() {
@@ -39,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function () {
         return card1Value === card2Value;
     }
     console.log(checkMatching)
-
     // Matched cards
     function handleMatch() {
         const [card1, card2] = flippedCards;
@@ -53,27 +54,91 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     console.log(handleMatch)
 
+     // Timer function
+     function startTimer() {
+        timerInterval = setInterval(function () {
+            if (secondsRemaining > 0) {
+                secondsRemaining--;
+                timer.textContent = secondsRemaining;
+            } else {
+                clearInterval(timerInterval);
+                endGame(); // End
+                function endGame() {
+                    clearInterval(timerInterval);
+                }
+            }
+        }, 1000);
+    }
+    console.log(startTimer)
+
+    function lockBoard() {
+        boardLocked = true;
+    }
+    
+    function unlockBoard() {
+        boardLocked = false;
+    }
+    // Reset 
+    function resetGame() {
+    resetFlippedCards();
+    matchedPairs = 0;
+    secondsRemaining = 120;
+    wrongMessage.style.display = "none";
+    clearInterval(timerInterval);
+    timer.textContent = secondsRemaining;
+    const shuffledCards = Array.from(cards);
+    shuffleArray(shuffledCards);
+    cards.forEach(card => {
+        card.classList.remove("matched");
+    });
+    startTimer();
+}
+console.log(resetGame)
+
+    // Reset flipped cards if no match
+    function resetFlippedCards() {
+        flippedCards.forEach(card => card.classList.remove("flipped"));
+        flippedCards = [];
+    }
+    console.log(resetFlippedCards)
+
+    //Start game
+    function startGame() {
+        if (gameStarted) {
+            return;
+        }
+        gameStarted = true;
+        startTimer();
+    const shuffledCards = Array.from(cards);
+    shuffleArray(shuffledCards);
+
     // End
     function endGame() {
         clearInterval(timerInterval);
     }
 
-    // Reset 
-    function resetGame() {
-        resetFlippedCards();
-        matchedPairs = 0;
-        secondsRemaining = 120;
-        wrongMessage.style.display = "none";
-        clearInterval(timerInterval);
-        timer.textContent = secondsRemaining;
-        const shuffledCards = Array.from(cards);
-        shuffleArray(shuffledCards);
-        cards.forEach(card => {
-            card.classList.remove("matched");
-        });
-        startTimer();
+
+    
+    
+
+
+    // Shuffle cards "Fisher-Yates"
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [cards[i], cards[j]] = [cards[j], cards[i]];
+        }
     }
-    console.log(resetGame)
+    console.log(shuffleArray)
+
+
+
+        //Event listeners
+    document.addEventListener("DOMContentLoaded", function () {
+        const cards = document.querySelectorAll(".dogcard");
+        cards.forEach(card => {
+            card.addEventListener("click", startGame);
+        });
 
     // Add click event listeners to cards
     cards.forEach(function (card) {
@@ -90,63 +155,9 @@ document.addEventListener("DOMContentLoaded", function () {
         unlockBoard(playAgainButton);
     });
 
-    // Timer function
-    function startTimer() {
-        timerInterval = setInterval(function () {
-            if (secondsRemaining > 0) {
-                secondsRemaining--;
-                timer.textContent = secondsRemaining;
-            } else {
-                clearInterval(timerInterval);
-                endGame();
-            }
-        }, 1000);
-    }
-    console.log(startTimer)
-
-    // Flip a card
-    function flipCard(card) {
-        if (boardLocked || card.classList.contains("flipped")) {
-            return;
-        }
-        card.classList.add("flipped");
-        flippedCards.push(card);
-
-        if (flippedCards.length === 2) {
-            lockBoard();
-            if (checkMatching()) {
-                handleMatch();
-            } else {
-                setTimeout(() => {
-                    resetFlippedCards();
-                    unlockBoard();
-                }, 1000);
-            }
-        }
-    }
-    console.log(flipCard)
-
-    // Start the game
-    startTimer();
-    const shuffledCards = Array.from(cards);
-    shuffleArray(shuffledCards);
-});
+   
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
