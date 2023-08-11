c9ef5149ce8e5c4dd838ae752f39d20da96eda11
Kateattardo <youremail@domain.com>
2023-08-11 01:23:01 -0400

    main.js

--- main.js
+++ main.js
@@ -3,16 +3,73 @@ const timer = document.getElementById("timer");
 const button = document.querySelector("#play-again-button");
 const wrongMessage = document.getElementById("wrong-message");
 
-let gameStarted = false;
+
 let flippedCards = [];
+let gameStarted = false;
 let matchedPairs = 0;
 let secondsRemaining = 120;
 let timerInterval;
 let lockedBoard = false;
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function shuffleAndRearrange() {
+    const shuffledCards = Array.from(cards);
+    shuffleArray(shuffledCards);
+    const cardContainer = cards[0].parentNode;
+    shuffledCards.forEach(card => {
+        cardContainer.appendChild(card);
+    });
+}
+
+
+function flipCard(card) {
+    card.classList.add('flipped');
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) {
+        if(checkMatch()) {
+            handleMatch();
+        } else {
+        setTimeout(resetCards, 1000);
+    }
+  }
+}
+
+function checkMatch() {
+    if (flippedCards.length === 2) {
+        const [card1, card2] = flippedCards;
+        return card1.dataset.card[0] === card2.dataset.card[0]; // checking if first letter of data-card is the same
+    }
+    return false;
+}
+
+function handleMatch() {
+    flippedCards[0].classList.add("matched");
+    flippedCards[1].classList.add("matched");
+    flippedCards = [];
+}
+
+function resetCards() {
+    flippedCards.forEach(card => card.classList.remove('flipped'));
+    flippedCards = [];
+}
+
+document.querySelector('.cards').addEventListener('click', function(event) {
+    const card = event.target.closest('.dogcard');
+    if (card && !card.classList.contains("flipped") && flippedCards.length < 2) {
+        flipCard(card);
+    }
+});
+
+
 function startGame() {
     if (gameStarted) return;
-
     gameStarted = true;
     shuffleAndRearrange();
     startTimer();
@@ -34,37 +91,8 @@ function shuffleAndRearrange() {
     });
 }
 
-function flipCard(card) {
-    if (lockedBoard || card.classList.contains("flipped")) return;
-    console.log (card)
-    card.classList.add("flipped");
-    flippedCards.push(card);
-
-    if (flippedCards.length === 2) {
-        lockTheBoard();
-        checkMatching() ? handleMatch() : setTimeout(resetCards, 1000);
-    }
-}
-
-function checkMatching() {
-    const [card1, card2] = flippedCards;
-    const card1Value = card1.dataset.card;
-    const card2Value = card2.dataset.card;
-    return card1Value === card2Value;
-}
-
-function handleMatch() {
-    flippedCards[0].classList.add("matched");
-    flippedCards[1].classList.add("matched");
-    flippedCards = [];
-    matchedPairs++;
-
-    if (matchedPairs === cards.length / 2) {
-        endGame();
-    } else {
-       console.log("reset")  
-        resetCards();
-        }
+function unlockTheBoard() {
+    lockedBoard = false;
 }
 
 function startTimer() {
@@ -78,13 +106,20 @@ function startTimer() {
     }, 1000);
 }
 
+function endGame() {
+    clearInterval(timerInterval);
+    if (matchedPairs === cards.length / 2) {
+        alert("Good dog");
+    } else {
+        alert("Nice try but no treat ");
+    }
+
+}
+
 function lockTheBoard() {
     lockedBoard = true;
 }
 
-function endGame() {
-    clearInterval(timerInterval);
-}
 
 function resetGame() {
     resetCards();
@@ -96,32 +131,39 @@ function resetGame() {
     startTimer();
 }
 
-function resetCards() {
+function resetCards(isWrongCardReset = true) {
     flippedCards.forEach(card => card.classList.remove("flipped"));
+    if(isWrongCardReset) {
+    wrongMessage.style.display = "block";
+    setTimeout(() => {
+        wrongMessage.style.display = "none";
+    }, 1000);
+  }
     flippedCards = [];
-    lockedBoard = false;
-}
+    
+   
+
+
 
 // Event Listeners
-cards.forEach(card => { 
-    console.log ("hello")
-    card.addEventListener("click", () => {
-        if (!card.classList.contains("flipped") && flippedCards.length < 2) {
-            console.log ("if flip")
+document.querySelector('.cards').addEventListener('click', function(event) {
+    const card = event.target.closest('.dogcard');
+        if (card && !card.classList.contains("flipped") && flippedCards.length < 2) {
             flipCard(card);
         }
     });
+
+
+document.querySelector(".board").addEventListener("click", function() {
+    if (!gameStarted) {
+        startGame();
+    }
 });
+
 
 button.addEventListener("click", () => {
     resetGame();
-    lockTheBoard();
+    unlockTheBoard();
 });
 
 
-
-
-
-
-
-   
