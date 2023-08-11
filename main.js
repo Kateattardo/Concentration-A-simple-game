756f955a63b6b1e3acb94a826a84cf9b8061015c
Kateattardo <youremail@domain.com>
2023-08-11 03:50:08 -0400

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,81 @@
+// Variables
 const cards = document.querySelectorAll(".dogcard");
+const timerDisplay = document.getElementById("timer");
+    let flippedCards = [];
+    let secondsRemaining = 120;
+    let timerInterval;
+
+// Event listener to handle card clicks
+document.querySelector('.cards').addEventListener('click', function(event) {
+    const card = event.target.closest('.dogcard');
+    if (card && !card.classList.contains("flipped") && flippedCards.length < 2) {
+        flipCard(card);
+    }
+});
+// Shuffle the cards
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function shuffleAndRearrange() {
+    const shuffledCards = Array.from(cards);
+    shuffleArray(shuffledCards);
+    const cardContainer = cards[0].parentNode;
+    const parentContainer = cardContainer.parentNode;
+    parentContainer.removeChild(cardContainer);
+    shuffledCards.forEach(card => {
+        cardContainer.appendChild(card);
+    });
+    parentContainer.appendChild(cardContainer);
+}
+
+shuffleAndRearrange();
+
+// Flip card function
+function flipCard(card) {
+    card.classList.add("flipped");
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) {
+        setTimeout(() => {
+            resetCards();
+        }, 1000);
+    }
+}
+
+// Reset cards 
+function resetCards() {
+    flippedCards.forEach(card => card.classList.remove("flipped"));
+    flippedCards = [];
+}
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        secondsRemaining--;
+        timerDisplay.textContent = formatTime(secondsRemaining);
+
+        if (secondsRemaining <= 0) {
+            clearInterval(timerInterval);
+            alert("Time's up!");
+        }
+    }, 1000);
+}
+
+function formatTime(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+}
+
+startTimer()
+
+
+
+
+/*const cards = document.querySelectorAll(".dogcard");
 const timer = document.getElementById("timer");
 const button = document.querySelector("#play-again-button");
 const wrongMessage = document.getElementById("wrong-message");
@@ -143,4 +220,4 @@ button.addEventListener("click", () => {
     resetGame();
     unlockTheBoard();
 });
-
+*/
